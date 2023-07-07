dd93733ce480602ccec8d09a6a10429e5ec5e128
Javier Anibal Villca <javieranibalvillca@gmail.com>
2023-07-07 01:38:45 -0300

    corregido .env

--- src/coches/utilities/cochesUtilities.js
+++ src/coches/utilities/cochesUtilities.js
@@ -1,5 +1,16 @@
 const crearId = () => Date.now();
 
+const crearCoche = (coche) => {
+  const { modelo, marca, precio, anio, descuento, es_0km, velocidad_crucero } = coche;
+  return {
+    modelo,
+    marca,
+    precio,
+    anio
+  };
+};
+
 module.exports = {
-  crearId
+  crearId,
+  crearCoche
 };
